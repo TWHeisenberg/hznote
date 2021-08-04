fe93c20b42896e9f2d1ded585855543d816aa0a1
TWang <1321716120@qq.com>
2021-08-04 14:16:42 +0800

    01doc\IE\java\算法\2021-08\[124]二叉树中的最大路径和.java 01doc\IE\java\算法\2021-08\[297]二叉树的序列化与反序列化.java

---
+++ 01doc/IE/java/算法/2021-08/[124]二叉树中的最大路径和.java
@@ -0,0 +1,83 @@
+//路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不
+//一定经过根节点。 
+//
+// 路径和 是路径中各节点值的总和。 
+//
+// 给你一个二叉树的根节点 root ，返回其 最大路径和 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,2,3]
+//输出：6
+//解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [-10,9,20,null,null,15,7]
+//输出：42
+//解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围是 [1, 3 * 104] 
+// -1000 <= Node.val <= 1000 
+// 
+// Related Topics 树 深度优先搜索 动态规划 二叉树 
+// 👍 1145 👎 0
+
+package editor.cn;
+public class BinaryTreeMaximumPathSum {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreeMaximumPathSum().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private int maxSum = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+      maxPathSum(root, 0);
+      return maxSum;
+    }
+
+    private int maxPathSum(TreeNode root, int sum){
+      if(root == null){
+        return 0;
+      }
+
+      // 如果是负的就不要
+      int leftMax = Math.max(0, maxPathSum(root.left, sum));
+      int rightMax = Math.max(0, maxPathSum(root.right, sum));
+
+      // 更新最大值
+      sum = leftMax + rightMax + root.val;
+      maxSum = Math.max(maxSum, sum);
+      // 返回单边最大的值
+      return root.val + Math.max(leftMax, rightMax);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
